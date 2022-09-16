d86e263b6dc3c095ffcb8bbdaa878594f78e5bcd
khsydney <khs10@hotmail.com>
2022-09-16 17:45:40 +0800

    adding branch test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello, World!! This is branch test-bug-with-line-selected"
 
 @app.route("/ping")
 def ping():
